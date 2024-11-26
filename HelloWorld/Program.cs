191e990ff16ee413d00e73041941b8100d92777d
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2024-11-26 11:11:47 +0700

    add new project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Tanapattara Wongkhamchan";
+string student_id = "670000000-0";
+// string is Datatype -> ชนิดของข้อมูล
+// name is variable
+// "..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id: " + student_id);
